18bc221912279dbc9696df1dd8045b56c9e14cdb
lodyaM <165099924+lodyaM@users.noreply.github.com>
2024-07-26 02:45:51 +0300

    Add files via upload

---
+++ JsonUpdater_ApplicationTests/AddCommandTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using JsonUpdater_Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonUpdater_Application.Tests
+{
+    [TestClass()]
+    public class AddCommandTests
+    {
+        [TestMethod()]
+        public void ExecuteAddCommandTest()
+        {
+            try
+            {
+                new AddCommand("FirstName:Ivan", "LastName:Ivanov", "Salary:100.50").Execute();
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail();
+            }
+
+
+        }
+    }
+}
